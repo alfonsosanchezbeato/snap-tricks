bfaa47397e97669d2ae5d2c8ad4707d2c464bbc0
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-07-16 17:48:57 +0200

    get-snap-version.sh: use store v2 API
    The new API allows us to get information about all available channels.

--- get-snap-version.sh
+++ get-snap-version.sh
@@ -1,22 +1,19 @@
-#!/bin/sh
-if [ $# -lt 2 ]
-then
-    echo "Usage:" "$0" "<snap-name> <channel(s)>"
+#!/bin/bash -e
+
+if [ $# -lt 1 ]; then
+    echo "Usage:" "$0" "<snap-name>"
     exit
 fi
 
-# TODO: start using
-# curl -s -H 'Snap-Device-Series: 16' https://api.snapcraft.io/v2/snaps/info/<snap_name>
-
 snap_name=$1
-shift
-for arch in amd64 arm64 i386 armhf
-do
-    for channel in "$@"
-    do
-        echo "$arch" in channel "$channel"
-        curl -s -H "X-Ubuntu-Series: 16" -H "X-Ubuntu-Architecture: $arch" \
-             https://search.apps.ubuntu.com/api/v1/snaps/details/"$snap_name"?channel="$channel" \
-            | jq '.' | grep "last_updated\|\"version\":\|revision"
-    done
+snap_info=$(curl -s -H 'Snap-Device-Series: 16' \
+            https://api.snapcraft.io/v2/snaps/info/"$snap_name")
+num_channel=$(echo "$snap_info" | jq -r '."channel-map" | length')
+
+for ((i = 0; i < "$num_channel"; ++i)); do
+    echo "$snap_info" | jq -r '."channel-map"['$i'] | .channel.name
+                                           + "\t" + .channel.architecture
+                                           + "\t" + .version
+                                           + "\t" + (.revision | tostring)
+                                           + "\t" + .channel."released-at"'
 done
