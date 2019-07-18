b418d87aee8437c44573c38000cdb9ce9bf8ffe2
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-07-18 15:01:18 +0200

    Simplify get-snap-version.sh by better using jq
    jq can iteratively apply a filter to all components of an array,
    leverage that so we do not need to loop in the shell.

--- get-snap-version.sh
+++ get-snap-version.sh
@@ -8,12 +8,8 @@ fi
 snap_name=$1
 snap_info=$(curl -s -H 'Snap-Device-Series: 16' \
             https://api.snapcraft.io/v2/snaps/info/"$snap_name")
-num_channel=$(echo "$snap_info" | jq -r '."channel-map" | length')
-
-for ((i = 0; i < "$num_channel"; ++i)); do
-    echo "$snap_info" | jq -r '."channel-map"['$i'] | .channel.name
+echo "$snap_info" | jq -r '."channel-map"[] | .channel.name
                                            + "\t" + .channel.architecture
                                            + "\t" + .version
                                            + "\t" + (.revision | tostring)
                                            + "\t" + .channel."released-at"'
-done
