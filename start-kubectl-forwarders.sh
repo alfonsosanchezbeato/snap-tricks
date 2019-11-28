67ed79fa37aff7d1a0820b5f8b2866220eb9bdaf
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-11-28 18:06:25 +0100

    start-kubectl-forwarders.sh: kill old forwarders

--- start-kubectl-forwarders.sh
+++ start-kubectl-forwarders.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -ex
 # bash needed for 'wait -n'
 
-if pgrep -f port-forward &> /dev/null; then
-    printf "Stop old forwarders before running this script\n"
-    exit 1
-fi
-
 pods=$(kubectl get pods | cut -f1 -d' ')
 mngm_pod=$(echo "$pods" | grep ^management-)
 ident_pod=$(echo "$pods" | grep ^identity-)
@@ -16,6 +11,15 @@ microk8s.status --wait-ready
 
 ports=(8010 8030 8883)
 pods=("$mngm_pod" "$ident_pod" "$mqtt_pod")
+
+# Kill old forwarders if present - we need to own the processes
+# for 'wait' to work
+i=0
+for port in "${ports[@]}"; do
+    signature="port-forward ${pods[$i]} --address 0.0.0.0 $port:$port"
+    pkill -f -- "$signature" || true
+    i=$((i + 1))
+done
 
 while true; do
     # Create forwarders as needed
