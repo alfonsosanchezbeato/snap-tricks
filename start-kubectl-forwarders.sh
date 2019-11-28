4e174da9c483ec564c94a96f2181c8b38a2397b4
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-11-28 17:43:14 +0100

    start-kubectl-forwarders.sh: keeps kubectl forwarders alive
    The kubectl forwarders tend to die because a network connection dies or
    for other random reasons. This script keeps them alive.

---
+++ start-kubectl-forwarders.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+# bash needed for 'wait -n'
+
+if pgrep -f port-forward &> /dev/null; then
+    printf "Stop old forwarders before running this script\n"
+    exit 1
+fi
+
+pods=$(kubectl get pods | cut -f1 -d' ')
+mngm_pod=$(echo "$pods" | grep ^management-)
+ident_pod=$(echo "$pods" | grep ^identity-)
+mqtt_pod=$(echo "$pods" | grep ^mqtt-)
+
+microk8s.start
+microk8s.status --wait-ready
+
+ports=(8010 8030 8883)
+pods=("$mngm_pod" "$ident_pod" "$mqtt_pod")
+
+while true; do
+    # Create forwarders as needed
+    i=0
+    for port in "${ports[@]}"; do
+        signature="port-forward ${pods[$i]} --address 0.0.0.0 $port:$port"
+        if ! pgrep -f -- "$signature" &> /dev/null; then
+            printf "not present for port: %s\n" "$port"
+            # shellcheck disable=SC2086
+            kubectl $signature &
+        fi
+        i=$((i + 1))
+    done
+
+    # Wait until a forwarder has died
+    wait -n || true
+done
