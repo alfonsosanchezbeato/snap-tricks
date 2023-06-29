732f3b359b993d9a62d8b3c56d320fa99e9d8809
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-06-29 08:05:35 +0100

    run-core.sh: find format from image file
    So we can support more than raw images.

--- run-core.sh
+++ run-core.sh
@@ -8,10 +8,12 @@ fi
 img=$1
 shift
 
+format=$(qemu-img info --output=json "$img" | jq -r .format)
+
 /usr/bin/qemu-system-x86_64 -enable-kvm -smp 2 -m 4096 \
     -bios /usr/share/OVMF/OVMF_CODE.fd \
     -netdev user,id=net0,hostfwd=tcp::8022-:22,hostfwd=tcp::31111-:31111 \
     -device virtio-net-pci,netdev=net0 \
-    -drive "file=$img",if=none,format=raw,id=disk1 \
+    -drive file="$img",if=none,format="$format",id=disk1 \
     -device virtio-blk-pci,drive=disk1,bootindex=1 \
     -serial mon:stdio "$@"
