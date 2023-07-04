730c17c0fa7688957de53bde16e0eaf7f7b2f6a2
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-07-04 14:58:44 +0100

    run-core.sh: make it possible to run UC16/18

--- run-core.sh
+++ run-core.sh
@@ -8,6 +8,13 @@ fi
 img=$1
 shift
 
+# For older UC: driver cannot be virtio-blk-pci as kernels did not include
+# that kernel module in the initramfs. Alternative: use "driver=ide-hd".
+disk_driver=virtio-blk-pci
+if sfdisk -d "$img" | grep 'name="writable"'
+then disk_driver=ide-hd
+fi
+
 format=$(qemu-img info --output=json "$img" | jq -r .format)
 
 /usr/bin/qemu-system-x86_64 -enable-kvm -smp 2 -m 4096 \
@@ -15,5 +22,5 @@ format=$(qemu-img info --output=json "$img" | jq -r .format)
     -netdev user,id=net0,hostfwd=tcp::8022-:22,hostfwd=tcp::31111-:31111 \
     -device virtio-net-pci,netdev=net0 \
     -drive file="$img",if=none,format="$format",id=disk1 \
-    -device virtio-blk-pci,drive=disk1,bootindex=1 \
+    -device "$disk_driver",drive=disk1,bootindex=1 \
     -serial mon:stdio "$@"
